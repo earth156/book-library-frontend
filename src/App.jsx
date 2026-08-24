fde8c5587f9f3960c2aa78774ddde39db8846a88
earth156 <162809249+earth156@users.noreply.github.com>
2026-08-24 23:01:31 +0700

    feat: configure app routing, main layout, global styles, and background image

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import ProtectedRoute from './components/ProtectedRoute';
 
 import LoginPage from './login/pages/LoginPage';
+import BooksPage from './books/pages/BooksPage';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="*" element={<Navigate to="/books" replace />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route
+          path="/books"
+          element={
+            <ProtectedRoute>
+              <BooksPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
